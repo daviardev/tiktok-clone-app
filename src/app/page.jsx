a4a71ec6a37b98588f3330998009d4ab1095fed0
daviardev <jerson.silva99@hotmail.com>
2023-10-14 23:36:55 -0500

    Render content with auth session

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,23 @@
 'use client'
 
-import { signIn, signOut, useSession } from 'next-auth/react'
-
 import MainLayout from './Layouts/MainLayout'
+
+import { useSession, signOut } from 'next-auth/react'
 
 export default function Home () {
   const { data: session } = useSession()
   return (
     <>
       <MainLayout>
-        <span className='text-white bg-red-600 h-12 w-12'>Hola</span>
-        {
-          !session
-            ? (
-              <button onClick={() => signIn()} className='text-white'>Iniciar sesión</button>
-              )
-            : (
-              <div>
-                <span className='text-white'>Bienvenido {session.user?.name}</span>
-                <button onClick={() => signOut()} className='text-white'>Cerrar sesión</button>
-              </div>
-              )
-        }
+        Contenido principal
+
+        Hola <span>{session.user?.name}</span>
+        <img
+          src={session.user?.image}
+          width={100}
+          height={100}
+        />
+        <button onClick={() => signOut()}>Cerrar sesión</button>
       </MainLayout>
     </>
   )
